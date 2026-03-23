7b37d776878a5eadf50ad0a01fb521a9e89c71d7
cmkonrad <cmargaretkonrad@gmail.com>
2026-03-23 15:23:38 -0700

    Added additional processing speed data. Calculated summaries and generated figures.

--- 01_scripts/02_software-trials-outcomes.R
+++ 01_scripts/02_software-trials-outcomes.R
@@ -19,6 +19,7 @@ rm(list = ls(all=TRUE))
 library(dplyr)
 library(ggplot2)
 library(lubridate)
+library(stringr)
 
 
 # ---- Read data ----
@@ -26,13 +27,11 @@ library(lubridate)
 dataBatches = read.csv("00_rawdata/04_SoftwareTrial_DataBatches.csv",stringsAsFactors = F)
 processingTimes = read.csv("00_rawdata/05_SoftwareTrial_ProcessingTimes.csv",stringsAsFactors = F)
 
-
-
-
 # ---- Format data ----
 
 dataBatches = dataBatches %>% 
-  select(Dataset,AudioLength_Days,SamplingFrequency_kHz)
+  select(Dataset,AudioLength_Days,SamplingFrequency_kHz,Channels) %>% 
+  mutate(Channels = as.factor(Channels))
 
 processingTimes = processingTimes %>% 
   left_join(dataBatches, relationship = "many-to-one") %>% 
@@ -42,11 +41,15 @@ processingTimes = processingTimes %>%
          ProcessingTime_minutes = End_datetime - Start_datetime,
          ProcessingSpeedFactor = AudioLength / ProcessingTime_minutes,
          TrialType = case_when(Software == "Triton" & methodVariant != "5s 100Hz resolution" ~ "WithinMethodVariant",
-                               Software == "Triton" & DecimateFactor != "1" ~ "WithinMethodVariant",
+                               Software == "Triton" & Approach == "LTSA" & DecimateFactor != "1" ~ "WithinMethodVariant",
+                               Software == "Triton" & Approach == "Click detector" & DecimateFactor == "1" ~ "WithinMethodVariant",
                                Software == "Triton" & Start == "20260311T0811" ~ "Repeatability",
                                Software == "Raven" & DecimateFactor == "1" ~ "WithinMethodVariant",
+                               Software == "PAMGuard" & (is.na(methodVariant) == F) ~ "WithinMethodVariant",
                                T ~ "Primary"
-                                       ))
+                                       ),
+         Method = case_when(Software %in% c("CABLE","Gubnitky and Diamant (2024)","detEdit") ~ Software,
+                            T ~ paste0(Software," - ", Approach)))
 
 # for processing that were done with pre-decimated files, add decimation time to total time
 totalProcessingTimes = processingTimes %>% 
@@ -67,7 +70,7 @@ totalProcessingTimes = processingTimes %>%
 
 ## ---- Within-Method variants ----
 
-### ---- Triton ----
+### ---- Triton LTSA ----
 
 TritonLTSA_subtrials = processingTimes %>% filter(Software == "Triton", Dataset == "Fundian Channel")
 
@@ -101,6 +104,25 @@ TritonLTSA_lowres = TritonLTSA_subtrials %>% filter(methodVariant == "20s 100Hz 
 
 ((TritonLTSA_lowres - TritonLTSA_benchmark) / TritonLTSA_benchmark) * 100
 # negligible difference (+2.7%) in processing time
+
+### ---- Triton SPICE Detector ----
+
+TritonSPICE_subtrials = processingTimes %>% 
+  filter(Software == "Triton", Approach %in% c("Click detector","Decimate"), Dataset == "Flemish Pass")
+
+TritonSPICE_benchmark = TritonSPICE_subtrials %>% filter(DecimateFactor == "1") %>% 
+  pull(ProcessingTime_minutes) %>% as.numeric()
+TritonSPICE_decimated = TritonSPICE_subtrials %>% filter(DecimateFactor == "4") %>% 
+  pull(ProcessingTime_minutes) %>% as.numeric()
+TritonDecimate = TritonSPICE_subtrials %>% filter(Approach == "Decimate") %>% 
+  pull(ProcessingTime_minutes) %>% as.numeric()
+
+# decimating: 
+
+((TritonSPICE_decimated - TritonSPICE_benchmark) / TritonSPICE_benchmark) * 100
+# 75% time saving once decimated
+((TritonSPICE_decimated + TritonDecimate - TritonSPICE_benchmark) / TritonSPICE_benchmark) * 100
+# and still 67% time saving when factoring the time required to decimate
 
 
 ### ---- Raven -----
@@ -141,10 +163,35 @@ cor(CABLEprocessing$ClicksFound, CABLEprocessing$ProcessingSpeedFactor)
 
 # ---- Plot data ----
 
+# Datasets
+
+dataBatches %>% 
+  ggplot() + geom_point(aes(AudioLength_Days,SamplingFrequency_kHz, 
+                            col = Dataset, pch = Channels), alpha = 0.7, 
+                        size = 4, stroke = 1.5, position = position_jitter(seed = 2, width = 0, height = 1))+
+  ylab("Sampling Frequency (kHz)")+xlab("Total Audio Duration (Days)")+
+  ylim(100,270)+xlim(0,8)+
+  theme_classic()+
+  scale_color_manual(values = c("#fdae61","#abd9e9","#d73027","#4575b4"))+
+  guides(col = guide_legend(override.aes = list(shape = 16)))
+
+ggsave("03_figures/Datasets.png", width = 6.5, height = 3, dpi = 300 )
+
+# Processing times
+
 totalProcessingTimes %>% 
-  ggplot() + geom_point(aes(Dataset,TotalSpeedFactor, col = Software), size = 4)+
-  ylab("Processing Speed (multiple of real time)")+
+  arrange(Method) %>% 
+  mutate(Method = case_when(Method == "Raven - BLED" ~ "Raven Pro - BLED",
+                            Method == "Triton - Click detector" ~ "Triton - SPICE detector",
+                            T ~ Method),
+         DatasetLabel = str_replace(Dataset," ","\n")) %>% 
+  ggplot() + geom_point(aes(DatasetLabel,TotalSpeedFactor, col = Method, pch = Method), size = 4, stroke = 1.5)+
+  ylab("Processing Speed\n(recording duration processed / processing time)")+xlab("Dataset")+
+  scale_y_continuous(limits = c(0, NA), expand = expansion(mult = c(0, 0.05)))+
+  scale_color_brewer(palette = "Paired")+
   theme_classic()
+
+ggsave("03_figures/ProcessingSpeed.png", width = 6.5, height = 5, dpi = 300 )
 
 CABLEprocessing %>% ggplot() + geom_point(aes(ClicksFound,ProcessingSpeedFactor))
 
